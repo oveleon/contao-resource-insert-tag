0be7ca57089c06484c8c4a6e75d1cb4b3487a96c
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2023-01-29 20:22:47 +0100

    [Update] Unlock PHP 8

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,12 +1,14 @@
 <?php
-array_insert($GLOBALS['BE_MOD']['content']['resource_insert_tags'], 0, array
-(
-    'tables' => array('tl_resource', 'tl_resource_tag')
-));
 
-// Register Model
-$GLOBALS['TL_MODELS']['tl_resource'] = 'Oveleon\\ContaoResourceInsertTag\\ResourceModel';
-$GLOBALS['TL_MODELS']['tl_resource_tag'] = 'Oveleon\\ContaoResourceInsertTag\\ResourceTagModel';
+use Contao\ArrayUtil;
+
+ArrayUtil::arrayInsert($GLOBALS['BE_MOD']['content']['resource_insert_tags'], 0, [
+    'tables' => ['tl_resource', 'tl_resource_tag']
+]);
+
+// Register Models
+$GLOBALS['TL_MODELS']['tl_resource']     = 'Oveleon\ContaoResourceInsertTag\ResourceModel';
+$GLOBALS['TL_MODELS']['tl_resource_tag'] = 'Oveleon\ContaoResourceInsertTag\ResourceTagModel';
 
 // Register hooks
-$GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array('contao_resource_insert_tag.listener.insert_tags', 'onReplaceInsertTags');
+$GLOBALS['TL_HOOKS']['replaceInsertTags'][] = ['contao_resource_insert_tag.listener.insert_tags', 'onReplaceInsertTags'];
